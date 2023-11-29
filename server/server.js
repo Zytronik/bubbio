0c268d34b8a9e82c804ab268cc1436de87ef4c1e
Zytronik <omar.oberholzer@gmail.com>
2023-11-29 13:38:57 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:8080", // Change this to your Vue.js app's URL
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Server</h1>');
+});
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (data) => {
+    console.log('Message from client:', data);
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>
+      <h2>Chat</h2>
+      <div v-for="msg in messages" :key="msg.id">
+        {{ msg.user }}: {{ msg.text }}
+      </div>
+      <div>
+        <input v-model="message" @keyup.enter="sendMessage" placeholder="Type your message" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import io from 'socket.io-client';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  setup() {
+    const messages = ref([]);
+    const message = ref('');
+    const socket = io('http://localhost:3000'); // Change the URL to your server address
+
+    const sendMessage = () => {
+      socket.emit('message', { user: 'User', text: message.value });
+      message.value = '';
+    };
+
+    socket.on('message', (data) => {
+      messages.value.push(data);
+    });
+
+    onMounted(() => {
+      console.log('Vue app mounted');
+    });
+
+    return {
+      messages,
+      message,
+      sendMessage,
+    };
+  },
 }
 </script>
 
 <style>
+#vueApp,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +60,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  width: 300px;
+  padding: 5px;
+  margin-top: 10px;
 }
 </style>
