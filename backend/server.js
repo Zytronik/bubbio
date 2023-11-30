6e63f04e30220784ce900def87c40b6f003ef426
Zytronik <omar.oberholzer@gmail.com>
2023-12-01 00:05:28 +0100

    cors again

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
+server.listen(3000, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,7 +21,7 @@ export default {
   setup() {
     const messages = ref([]);
     const message = ref('');
-    const socket = io('http://localhost:8080', { 
+    const socket = io('https://bubbio-2qce6.ondigitalocean.app:3000', { 
       withCredentials: false 
     });
 
