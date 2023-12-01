3a748a691444cf4827d467b79f5257f7e140e845
Zytronik <omar.oberholzer@gmail.com>
2023-12-01 18:21:29 +0100

    cors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,7 +21,7 @@ export default {
   setup() {
     const messages = ref([]);
     const message = ref('');
-    const socket = io('/bubbio-backend');
+    const socket = io({"url" : 'https://bubbio-2qce6.ondigitalocean.app/bubbio-backend'}, {});
 
     const sendMessage = () => {
       socket.emit('message', { user: 'User', text: message.value });
