c60569532df9b1974b2d36323412112565f9cbcd
Zytronik <omar.oberholzer@gmail.com>
2023-12-02 11:27:05 +0100

    refactor

--- frontend/src/pages/Home/App.vue
+++ frontend/src/pages/Home/App.vue
@@ -24,7 +24,7 @@ export default {
     const host = window.location.host;
     let serverURL = "";
     let ioOptions = {
-      path: "/bubbio-backend/socket.io",
+      path: "/blubbio-backend/socket.io",
     }
     if(host === "localhost:8080"){
       serverURL = "http://localhost:3000/";
