99534e740c80db8164ff62695244d1e626946f00
simitomorrow <email@michelsimon.ch>
2023-12-08 15:11:39 +0100

    trying out sockets for gameplay

--- backend/game.js
+++ backend/game.js
@@ -1,12 +1,35 @@
-function setup(io){
+const POSSIBLE_COLORS = [0, 1, 2, 3, 4, 5, 6];
+let bubbleQueue = genereateBubbleQueue(100);
+
+function genereateBubbleQueue(queueLength) {
+    let resultQueue = "";
+    for (let i = 0; i < queueLength; i++) {
+        resultQueue += POSSIBLE_COLORS[Math.random(POSSIBLE_COLORS.length)];
+    }
+    console.log(resultQueue);
+    return resultQueue;
+}
+
+function setup(io) {
     io.on('connection', (socket) => {
-        setInterval(() => {
-            console.log("test");
-            socket.emit('generateQueue', { queue: "test123" });
-        }, 2000);
+        socket.emit('generateQueue', { queue: "test123" });
+
+        socket.on('testma', (data) => {
+            console.log(data);
+            socket.emit("testma", { th: "hello" });
+        });
     });
 }
 
 module.exports = {
     setup: setup,
 };
+
+/*
+queue
+current piece
+hold piece
+board
+arrow angle
+current combo
+*/
--- frontend/src/pages/Game/game.vue
+++ frontend/src/pages/Game/game.vue
@@ -1,29 +1,79 @@
 <template>
   <div id="game" class="page">
     <h1>Game</h1>
+    <span>queue: {{ queue }}</span> <br>
+    <span>currentBubble: {{ currentBubble }}</span> <br>
+    <span>holdBubble: {{ holdBubble }}</span> <br>
+    <span>board: {{ board }}</span> <br>
+    <span>angle: {{ angle }}</span> <br>
+    <span>currentCombo: {{ currentCombo }}</span> <br>
+  </div>
+  <hr>
+  <div>
+    <span>sentPackages: </span>
+    <p>{{ sentPackages }}</p>
+  </div>
+  <hr>
+  <div>
+    <span>receivedPackages: </span>
+    <p>{{ receivedPackages }}</p>
+  </div>
+  <hr>
+  <div>
+    <button @click="testma">testma</button>
   </div>
 </template>
 
 <script>
 import { socket } from '../../clientWebsocket.js';
-import { onMounted  } from 'vue';
+import { ref, onMounted } from 'vue';
 
 export default {
   name: 'App',
   setup() {
+    let queue = ref("bro\nbroo");
+    let currentBubble = ref("nice");
+    let holdBubble = ref("meme");
+    let board = ref("haha");
+    let angle = ref("all i need");
+    let currentCombo = ref("for christmas");
+
+    let sentPackages = ref("");
+    let receivedPackages = ref("");
 
     onMounted(() => {
       console.log('Vue app mounted | game');
       socket.on('generateQueue', (data) => {
         console.log(data);
       });
-
     });
+
+    socket.on('testma', (data) => {
+      receivedPackages.value += "\n" + data;
+      console.log(data);
+    });
+
+    const testma = () => {
+      sentPackages.value += "\ntestma";
+      console.log(sentPackages.value)
+      socket.emit('testma', { pog: "asdf" });
+    };
+
+    return {
+      queue,
+      currentBubble,
+      holdBubble,
+      board,
+      angle,
+      currentCombo,
+      sentPackages,
+      receivedPackages,
+
+      testma,
+    };
 
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
