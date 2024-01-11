fdc234b4c2c8db9cd6333e868120b50fe114f0a8
Zytronik <omar.oberholzer@gmail.com>
2024-01-11 11:26:41 +0100

    do

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { User } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator';
+import { Controller } from '@nestjs/common';
+//import { User } from '@prisma/client';
+//import { GetUser } from 'src/auth/decorator';
 
 @Controller('users')
 export class UserController {
-    @Get('me')
-    getMe(@GetUser() user: User, @GetUser('email') email: string){
+
+    //@Get('me')
+    //getMe(@GetUser() user: User, @GetUser('email') email: string){
         //console.log(email);
-        return user;
-    }
+       // return user;
+   // } 
 }
