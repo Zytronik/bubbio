714702314d7c6eea8a02ca8b80e47deef860fd49
Zytronik <omar.oberholzer@gmail.com>
2024-01-16 14:26:42 +0100

    here we go again

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { LobbyGateway } from './lobby/lobby.gateway';
+import { LobbyGateway } from './lobby/lobby.data.gateway';
 import { GameGateway } from './game/game.gateway';
 
 @Module({
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@nestjs/common";
 
-
 @Injectable()
 export class AuthService {
-  constructor(
-
-  ) { }
+    constructor( ) {}
+  
 }
