5ba8e626b0f8fb7b4fcc37e69d7b1b6321523c58
Zytronik <omar.oberholzer@gmail.com>
2024-01-26 01:34:44 +0100

    maybe this

--- frontend/src/ts/networking/networking.http-client.ts
+++ frontend/src/ts/networking/networking.http-client.ts
@@ -1,6 +1,17 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-export const httpClient = axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: true
-});
+
+const host: string = window.location.host;
+export let httpClient: AxiosInstance;
+
+if (host === "localhost:8080") {
+    httpClient = axios.create({
+        baseURL: 'http://localhost:3000',
+        withCredentials: true
+    });
+}else{
+    httpClient = axios.create({
+        baseURL: 'https://blubb.io/blubbio-backend/',
+        withCredentials: true
+    });
+}
