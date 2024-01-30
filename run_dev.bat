a13b737569ed309dc4a41d29aa76a970f9e3c2d2
simitomorrow <email@michelsimon.ch>
2024-01-30 18:09:17 +0100

    better run_dev,bat

--- run_dev.bat
+++ run_dev.bat
@@ -2,4 +2,4 @@
 
 start cmd /k "cd .\frontend && npm run dev"
 start cmd /k "cd .\backend && npm run start:dev"
-start cmd /k "cd .\backend && npm run db:dev:up & timeout /t 2 && npm run prisma:start && npx prisma studio"
+start cmd /k "cd .\backend && docker start backend-dev-db-1 && npm run prisma:start && npx prisma studio"
