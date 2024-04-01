3e87c846bab6830a1f42d86f3192dd5852c48fd8
Zytronik <omar.oberholzer@gmail.com>
2024-04-01 16:41:22 +0200

    fix

--- frontend/src/pages/ranked/RankedPage.vue
+++ frontend/src/pages/ranked/RankedPage.vue
@@ -22,7 +22,7 @@
             </div>
 
             <div v-if="playerStats" class="progressBarWrapper">
-              <div v-if="playerStats.rankInfo.prevRank" class="prevRank">
+              <div v-if="playerStats.rankInfo.prevRank?.ascii" class="prevRank">
                 <p v-if="playerStats.rankInfo.percentile">{{ playerStats.rankInfo.percentile }}%</p>
                 <p v-if="playerStats.rankInfo.prevRank.ascii" class="rank-letter">{{ playerStats.rankInfo.prevRank.ascii
                   }}</p>
