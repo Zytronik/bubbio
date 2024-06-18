8ca7b308fe6de0169ad20765a26b9a76f0961c16
Zytronik <omar.oberholzer@gmail.com>
2024-06-18 15:45:03 +0200

    global stats

--- backend/src/sprint/sprint.service.ts
+++ backend/src/sprint/sprint.service.ts
@@ -145,6 +145,41 @@ export class SprintService {
         return this.prisma.sprint.count();
     }
 
+    async getTotalSprintDuration() {
+        const totalSprintDuration = await this.prisma.sprint.aggregate({
+            _sum: {
+                gameDuration: true,
+            },
+        });
+        return totalSprintDuration._sum.gameDuration;
+    }
+
+    async getTotalRankedDuration(): Promise<number> {
+        // Fetch all ranked games from the database
+        const rankedGames = await this.prisma.ranked.findMany();
+    
+        // Calculate the total duration
+        let totalDuration = 0;
+        for (const game of rankedGames) {
+            const user1Stats: GameStats = JSON.parse(game.user1Stats);
+            const user2Stats: GameStats = JSON.parse(game.user2Stats);
+            if(user1Stats[0] && user2Stats[0]){
+                totalDuration += user1Stats[0].gameDuration;
+                totalDuration += user2Stats[0].gameDuration;
+            }
+        }
+        
+        return totalDuration;
+    }
+
+    async getGlobalStats(){
+        const totalSprintDuration = await this.getTotalSprintDuration();
+        const totalRankedDuration = await this.getTotalRankedDuration();
+        const totalPlayTime = totalSprintDuration + totalRankedDuration;
+        const totalGames = await this.getTotalGamesPlayed();
+        return {totalPlayTime, totalGames};
+    }
+
     async getTotalGamesPlayedByUser(userId: number): Promise<number> {
         return this.prisma.sprint.count({
             where: {
