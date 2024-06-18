4fffce6b6c57cf37fc908a46badfeb22dcc9ff23
simitomorrow <email@michelsimon.ch>
2024-06-18 15:20:20 +0200

    compressed replays

--- backend/src/game/i/game.i.game-state-history.ts
+++ backend/src/game/i/game.i.game-state-history.ts
@@ -33,3 +33,42 @@ export interface AngleFrame {
     frameTime: number,
     angle: number,
 }
+
+export interface CompressedGameStateHistory {
+    i: CompressedInputFrame[], // inputHistory
+    b: CompressedBoardHistoryFrame[], // boardHistory
+    q: CompressedBubbleQueueFrame[], // bubbleQueueHistory
+    a: CompressedAngleFrame[], // angleHistory
+}
+
+interface CompressedInputFrame {
+    n: number,
+    t: number,
+    i: GAME_INPUT,
+    a: number,
+    g: number,
+}
+interface CompressedBoardHistoryFrame {
+    t: number,
+    b: string,
+}
+interface CompressedBubbleQueueFrame {
+    t: number,
+    c: Bubble,
+    h?: Bubble,
+    q: number,
+    g: number,
+}
+interface CompressedAngleFrame {
+    t: number,
+    a: number,
+}
+
+export function compressReplayData(history: GameStateHistory): CompressedGameStateHistory {
+    return {
+        i: history.inputHistory.map(frame => ({ n: frame.indexID, t: frame.frameTime, i: frame.input, a: frame.angle, g: frame.garbageAmount })),
+        b: history.boardHistory.map(frame => ({ t: frame.frameTime, b: frame.boardState })),
+        q: history.bubbleQueueHistory.map(frame => ({ t: frame.frameTime, c: frame.currentBubble, h: frame.heldBubble, q: frame.queueSeedState, g: frame.garbageSeedState })),
+        a: history.angleHistory.map(frame => ({ t: frame.frameTime, a: frame.angle })),
+    };
+}
